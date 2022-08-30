9c3812047742b686ac15172831418533e66beb47
jshimaru <55031303+JShimaru@users.noreply.github.com>
2022-08-30 16:11:45 -0400

    updated

--- boom-server/db/index.js
+++ boom-server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/boomUsers'
+let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb+srv://Jirama:DatabaseConnect@cluster0.kiculqi.mongodb.net/boomUsers' || 'mongodb://127.0.0.1:/boomUsers'
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true, useNewUrlParser: true
