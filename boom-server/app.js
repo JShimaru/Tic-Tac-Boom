cc7a0fdd3b096cb9e69ef45ab1843f75cff0f048
jshimaru <55031303+JShimaru@users.noreply.github.com>
2022-08-30 18:32:18 -0400

    updated scripts for deployment

--- boom-server/app.js
+++ boom-server/app.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 app.use(logger('dev'))
